6acd900512169c1a2e3dfc29ab11f2343f190727
marius <marius.contoli-legay@epitech.eu>
2020-05-05 16:26:27 +0200

    added header file with macros of n4s commands

---
+++ include/n4s.h
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2020
+** n4s.h
+** File description:
+** n4s header
+*/
+
+#ifndef N4S_H_
+#define N4S_H_
+
+#define START "START_SIMULATION"
+#define STOP "STOP_SIMULATION"
+#define FOR "CAR_FORWARD:"
+#define BACK "CAR_BACKWARDS:"
+#define WHEELS "WHEELS_DIR:"
+#define INFO "GET_INFO_LIDAR"
+#define G_SPEED "GET_CURRENT_SPEED"
+#define G_WHEELS "GET_CURRENT_WHEELS"
+#define CYCLE "CYCLE_WAIT:"
+#define MAX_SPEED "GET_CAR_SPEED_MAX"
+#define MIN_SPEED "GET_CAR_SPEED_MIN"
+#define TIME "GET_INFO_SIMTIME"
+
+#endif /* !N4S_H_ */
