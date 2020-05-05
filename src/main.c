6220495b25fbeeac25a2501c9f1c485acf3ada3d
marius <marius.contoli-legay@epitech.eu>
2020-05-05 17:17:45 +0200

    implemented two commands : start and stop simulation; launching pipes.sh for testing purpose, the programm is detecting well

--- src/main.c
+++ src/main.c
@@ -9,7 +9,13 @@
 
 void start(char *str, size_t len)
 {
-    put_command(START);
+    put_command(START"\n");
+    getline(&str, &len, stdin);
+}
+
+void stop(char *str, size_t len)
+{
+    put_command(STOP"\n");
     getline(&str, &len, stdin);
 }
 
@@ -17,8 +23,9 @@ int main(void)
 {
     char *str = NULL;
     size_t len = 0;
-    
-    start(str, len);
 
-    return (84);
+    start(str, len);
+    stop(str, len);
+
+    return (0);
 }
