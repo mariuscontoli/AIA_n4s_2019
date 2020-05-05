7b40203eb103255398869f7d4d48678e239eada1
marius <marius.contoli-legay@epitech.eu>
2020-05-05 18:24:59 +0200

    implemented start function; need str to word tab to continue the project

--- include/n4s.h
+++ include/n4s.h
@@ -11,7 +11,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+#include <assert.h>
 
 #define START "START_SIMULATION"
 #define STOP "STOP_SIMULATION"
--- src/main.c
+++ src/main.c
@@ -25,17 +25,43 @@ void get_infos(char *str, size_t len)
     getline(&str, &len, stdin);
 }
 
+void car_forward(char *str, size_t len)
+{
+    put_command(FOR"0.3\n");
+    getline(&str, &len, stdin);
+}
+
+int is_track_cleared(char *str)
+{
+    char **tab = NULL;
+    int i = 0;
+
+    /* INSERT STR TO WORD ARRAY */
+    
+    while (tab[i] != NULL) {
+        if (strcmp("Track Cleared", tab[i])) {
+            put_command(STOP"\n");
+            return (0);
+        }
+        i += 1;
+    }
+    return (1);
+}
+
 int main(void)
 {
     char *str = NULL;
     size_t len = 0;
+    char **infos = NULL;
+    int offset = 1;
 
     start(str, len);
-    while (1)
+    car_forward(str, len);
+    while (offset)
     {
         get_infos(str, len);
+        offset = is_track_cleared(str);
     }
-    
     stop(str, len);
 
     return (0);
