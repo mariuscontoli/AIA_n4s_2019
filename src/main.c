6a0a27857abce90d8f791e4289f062de05e427dd
KURILOV Konstantin <konstantin.kurilov@epitech.eu>
2020-05-06 14:56:19 +0200

    remve includes

--- src/main.c
+++ src/main.c
@@ -5,12 +5,19 @@
 ** main source file
 */
 
-#include "../include/n4s.h"
+#include <my_tools.h>
+#include <n4s.h>
+
+int remov_space(char const *c)
+{
+    int i = 0;
+    for (; (c[i] == ':' || c[i] == '\t') && c[i] != '\0'; i++);
+    return i;
+}
 
 int main(void)
 {
-
-
-    
-    return (84);
+    char **tab = my_str_to_word_array("bili:bili:fasfsad:safsadf:f:sdfs", remov_space);
+    my_show_word_array(tab);
+    return (0);
 }
